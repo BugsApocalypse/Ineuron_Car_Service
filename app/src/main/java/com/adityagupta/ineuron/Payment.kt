3e7605315d824bf393f3a631627fcc2116e07fd0
Rahul Raj <rahuldps3313@gmail.com>
2022-06-18 16:39:35 +0530

    payment

---
+++ app/src/main/java/com/adityagupta/ineuron/Payment.kt
@@ -0,0 +1,15 @@
+package com.adityagupta.ineuron
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Payment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+
+
+
+
+}
