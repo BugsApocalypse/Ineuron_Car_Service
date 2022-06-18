bb091592250f423050c6c01395c955111f1efc1f
Rahul Raj <rahuldps3313@gmail.com>
2022-06-18 15:15:49 +0530

    service centre info page

--- app/src/main/java/com/adityagupta/ineuron/ServiceCentreInfoActivity.kt
+++ app/src/main/java/com/adityagupta/ineuron/ServiceCentreInfoActivity.kt
@@ -3,10 +3,9 @@ package com.adityagupta.ineuron
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ServiceCentreInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_service_centre_info)
     }
 }
-
